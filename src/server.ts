724d5dbf3a0db00f6881351a76ee1f6387f02c06
toaaa <toa@toaaa.de>
2023-11-28 18:53:01 +0100

    setup folders on startup

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,28 @@
 import express, { Express, Request, Response, NextFunction } from 'express';
-import { PORT } from './utils/constants';
-import uploadRoutes from './routes/uploadRoutes';
-import imageRoutes from './routes/imageRoutes';
+import { PORT, uploadsDir, sharexDir } from './utils/constants';
 import { addTimestamps } from './middlewares/addTimestamps';
 import { getMetrics } from './middlewares/metrics';
 import { getTime } from './middlewares/time';
+import uploadRoutes from './routes/uploadRoutes';
+import imageRoutes from './routes/imageRoutes';
+import fs from 'fs';
+import path from 'path';
 
 const app: Express = express();
+
+// Function to check and create the folder structure
+const setupFolders = () => {
+  const fullUploadsDir = path.join(uploadsDir, sharexDir);
+
+  // Check if the directory exists, if not, create it
+  if (!fs.existsSync(fullUploadsDir)) {
+    fs.mkdirSync(fullUploadsDir, { recursive: true });
+    console.log(`Created folder structure: ${fullUploadsDir}`);
+  }
+};
+
+// Call the setup function
+setupFolders();
 
 // Apply middleware
 app.use(express.json()); // Parse JSON requests
