11c3cddb3e64a933547055086dca473388d5d831
toaaa <toa@toaaa.de>
2024-02-05 18:55:53 +0100

    feat: formatTime helper function

---
+++ src/utils/formatTime.ts
@@ -0,0 +1,10 @@
+﻿import { format, fromUnixTime, isDate } from 'date-fns';
+
+export const formatTime = (timestampOrDate: number | Date): string => {
+  const date = isDate(timestampOrDate) ? timestampOrDate : fromUnixTime(timestampOrDate);
+  const formattedDate = format(date, 'dd.MM.yyyy / HH:mm:ss');
+
+  return formattedDate;
+};
+
+export default formatTime;
