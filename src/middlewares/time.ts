cb0ce120e336a1d1d605f8b7914a6013698ca20f
toaaa <toa@toaaa.de>
2024-02-29 23:30:19 +0100

    Update time.ts

--- src/middlewares/time.ts
+++ src/middlewares/time.ts
@@ -4,7 +4,5 @@ export const getTime = (req: Request, res: Response, next: NextFunction) => {
   const currentTimestamp = new Date().toISOString();
   const unixTime = Math.floor(Date.now() / 1000);
 
-  res.status(200).json({ timestamp: currentTimestamp, timestamp_unix: unixTime });
-
-  next();
+  return res.status(200).json({ timestamp: currentTimestamp, timestamp_unix: unixTime });
 };
